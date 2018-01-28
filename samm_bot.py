ed185777342006cc4398632d3a924bf5063db9f5
zeo <zeo@localhost.f27>
2018-01-28 15:15:29 -0600

    First commit

--- samm_bot.py
+++ samm_bot.py
@@ -1,10 +1,17 @@
 from telegram.ext import Updater, CommandHandler
 import web
 
+'''
 db_host = 'localhost'
 db_name = 'bot_asistencia'
 db_user = 'kuorra'
 db_pw = 'kuorra.2017'
+'''
+
+db_host = 'fugfonv8odxxolj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_name = 'eftaz6n2sqv292f7'
+db_user = 'yg9mvid1uls20vfz'
+db_pw = 'abdhhbjou44g6l4k'
 
 db = web.database(
     dbn='mysql',
